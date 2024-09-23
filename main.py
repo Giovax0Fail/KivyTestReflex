9c43f0067f197486d671b870cc9ba03f870fe9bc
Giovax0Fail <g.ferrante5@outlook.com>
2024-09-23 13:25:01 +0200

    loll

---
+++ main.py
@@ -0,0 +1,38 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDRaisedButton
+
+
+class Reflex(MDApp):
+    def build(self):
+        self.title = "Reflex"
+        self.theme_cls.primary_palette = "Red"
+        self.theme_cls.primary_hue = "200"
+        return Builder.load_file("main.kv")
+    
+    dialog = None
+
+    def on_start(self):
+        self.show_dialog()
+    def show_dialog(self):
+        if not self.dialog:
+            self.dialog = MDDialog(
+                title="Testa i Tuoi riflessi",
+                text="Metti alla prova i tuoi riflessi con la nostra app! \nMisura la tua velocità di reazione e confronta i tuoi tempi con la media umana di circa 250 millisecondi per gli stimoli visivi\nSfida te stesso e i tuoi amici per vedere chi ha i riflessi più rapidi!",
+                buttons=[
+                    MDRaisedButton(
+                        text="CHIUDI",
+                        on_release=self.close_dialog
+                    ),
+                ],
+            )
+        self.dialog.open()
+
+    def close_dialog(self, *args):
+        self.dialog.dismiss()
+
+
+if __name__ == '__main__':
+    Reflex().run()
